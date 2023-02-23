14c01aaab6f85ccc62991b1bb859b04e71c28753
lyf <luyifeng3@163.com>
2023-02-23 15:35:01 +0800

    add wrapper file

--- wrapper.go
+++ wrapper.go
@@ -1,2 +1,33 @@
 package wrapper
 
+import (
+	"net/http"
+	"net/url"
+)
+
+type WrapperClient struct {
+	Client *http.Client
+	ApiUrl string
+}
+
+func NewWrapperClient() *WrapperClient {
+	return &WrapperClient{}
+}
+
+func (c *WrapperClient) SetAPIUrl(apiUrl string) error {
+	_, err := url.ParseRequestURI(apiUrl)
+	if err != nil { // the api url is wrong
+		return err
+	}
+	c.ApiUrl = apiUrl
+	return nil
+}
+
+func (c *WrapperClient) SetProxy(proxyUrl string) error {
+	proxy, err := url.ParseRequestURI(proxyUrl)
+	if err != nil {
+		return err
+	}
+	c.Client.Transport = &http.Transport{Proxy: http.ProxyURL(proxy)}
+	return nil
+}
